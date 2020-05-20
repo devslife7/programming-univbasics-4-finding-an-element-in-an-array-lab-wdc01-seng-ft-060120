e994a3060152ec87318ad5796fff1fef8b557e07
Marcos Velasco <devslife7@gmail.com>
2020-05-20 00:54:54 +0000

    Automatically backed up by Learn

--- lib/find_in_array.rb
+++ lib/find_in_array.rb
@@ -1,11 +1,9 @@
 def find_element_index(array, value_to_find)
   count = 0
-  index = 0
   while count < array.length do
     if (array[count] == value_to_find)
       return array[count].intex
     end
     count += 1
   end
-  index
 end
